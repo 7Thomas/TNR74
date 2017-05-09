b06f3b7c514c79f7d4d10f1caf2190c8dc09a144
7Thomas <tr92801@gmail.com>
2017-05-09 16:04:43 -0700

    Add files via upload

---
+++ js/deaths.js
@@ -0,0 +1,448 @@
+
+var deaths =
+
+{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Monica Loera ",
+        "Age": 43,
+        "ETHNICITY": "LATINA ",
+        "Identity": "Transgender Woman ",
+        "TypeofDeath": "Fatal Gun Shot ",
+        "imAgeurl ": "http://www.advocate.com/sites/advocate.com/files/2016/02/01/loerarowell_750.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -97.742116,
+          30.413854
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Jasmin Sierra ",
+        "Age": "Unknown",
+        "ETHNICITY": "LATINA ",
+        "Identity": "Transgender Woman ",
+        "TypeofDeath": "Unknown ",
+        "imAgeurl ": "https://3.bp.blogspot.com/-pD8eKU6Gcqk/Vz4SUZ9AafI/AAAAAAABMd8/wMQ6IXwmJlg-J-vLT57Elo4Wpzx8b-RKACKgB/s1600/9.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -119.066782,
+          35.359862
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Maya Young ",
+        "Age": 25,
+        "ETHNICITY": "BLACK ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Stabbed ",
+        "imAgeurl ": "http://media.nbcphiladelphia.com/images/1200*675/Transgeder+Woman+Murder+Jose+Pena+Maya+Young+Tiffany+Floyd.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -75.07974,
+          40.017017
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Demarkis Stansberry ",
+        "Age": 30,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Man ",
+        "TypeofDeath": "Fatal Gun Shot ",
+        "imAgeurl ": "http://blackyouthproject.com/wp-content/uploads/2016/03/Demarkis-Stansberry-1.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -91.129957,
+          30.441997
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Kendarie Johnson ",
+        "Age": 16,
+        "ETHNICITY": "Black ",
+        "Identity": "Gender Fluid ",
+        "TypeofDeath": "Fatal Gun shot ",
+        "imAgeurl ": "https://www.gannett-cdn.com/-mm-/4239397806da815a22d9c49856af228300718be1/c=0-109-2555-3516&r=295&c=0-0-292-390/local/-/media/2016/03/11/IAGroup/DesMoines/635933040571663551-0312-Kedarie-Johnson-13.JPG",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -91.126847,
+          40.804323
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Quartney  Davia Dawson-Yochum ",
+        "Age": 32,
+        "ETHNICITY": "Mixed ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Fatal Gun shot ",
+        "imAgeurl ": "http://www.pghlesbian.com/wp-content/uploads/2016/03/kourtney.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -118.244361,
+          34.04438
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Shante Thompson ",
+        "Age": 34,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Fatal Gun Shot ",
+        "imAgeurl ": "http://assets.nydailynews.com/polopoly_fs/1.2595892.1460374178!/img/httpImAge/imAge.jpg_gen/derivatives/article_1200/trans12n-1-web.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -95.374346,
+          29.743917
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Keyonna Blakeney ",
+        "Age": 22,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Traumatic Force ",
+        "imAgeurl ": "http://media.nbcnewyork.com/images/041816+keyonna+blakeney.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.181507,
+          39.119009
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Reecey Walker ",
+        "Age": 32,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Stabbed to death ",
+        "imAgeurl ": "http://www.pghlesbian.com/wp-content/uploads/2016/05/tyreece-walker-3-x750-300x225.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -97.373114,
+          37.642417
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Mercedes Successful ",
+        "Age": 32,
+        "ETHNICITY": " Black ",
+        "Identity": "Transgedner Womyn ",
+        "TypeofDeath": "Fatal Gun Shot ",
+        "imAgeurl ": "http://images.dailykos.com/images/253674/story_imAge/mercedes-successful-x750.jpg?1463779851",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.617918,
+          28.101309
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Amos Beede ",
+        "Age": 38,
+        "ETHNICITY": "White ",
+        "Identity": "Transgender Man ",
+        "TypeofDeath": "Traumatic Force ",
+        "imAgeurl ": "http://www.metroweekly.com/wp-content/uploads/2016/06/Amos-Beede.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -73.218085,
+          44.468702
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Goddess Diamond ",
+        "Age": 20,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Blunt Force/Arson ",
+        "imAgeurl ": "http://www.advocate.com/sites/advocate.com/files/2016/06/15/diamond.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -89.977235,
+          30.030487
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Deeniquia Dodds ",
+        "Age": 22,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Fatal Gun Shot wound ",
+        "imAgeurl ": "https://thumbs.mic.com/OGQxMmE3ODEwYiMvZW1qSEtHNl91eVNCZXpVU1N3TmF4UVJHSFBJPS81OHgzNToyMzQyeDEyMDAvMTYwMHg5MDAvZmlsdGVyczpmb3JtYXQoanBlZyk6cXVhbGl0eSg4MCkvaHR0cHM6Ly9zMy5hbWF6b25hd3MuY29tL3BvbGljeW1pYy1pbWFnZXMvcTh5eWZ1ZXpmdnBwenR4YnlvZHV0bmpubmcxcndoZ3pxNHVxY2R2dmt1cW9xcHB2Z2Q4ZXJwYm1na3FjdTF0ay5qcGc.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.986996,
+          38.89525
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Dee Whigham ",
+        "Age": 23,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Stab wounds ",
+        "imAgeurl ": "http://www.sunherald.com/news/local/isc6lr/picture92589472/alternates/FREE_640/Dee%20Whigham",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -88.864489,
+          30.439666
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Erykah Tijerina ",
+        "Age": 36,
+        "ETHNICITY": "Latina ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Fatal Gun Shot ",
+        "imAgeurl ": "http://assets.nydailynews.com/polopoly_fs/1.2792338.1473877005!/img/httpImAge/imAge.jpg_gen/derivatives/article_750/bliss15n-2-web.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -106.478343,
+          31.779193
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Rae'Lynn Thomas ",
+        "Age": 28,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Beating / Fatal Gun Shot ",
+        "imAgeurl ": "http://www.advocate.com/sites/advocate.com/files/2016/08/12/raelynn-thomas-750x563.jpg",
+        "City_State ": "Columbus ",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -82.986617,
+          39.943667
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "T.T.  Saffore ",
+        "Age": "26-27 ",
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Fatal Gun Shot ",
+        "imAgeurl ": "https://lgbtqnation-assets.s3.amazonaws.com/assets/2016/09/IMG_0308-500x497.jpg",
+        "City_State ": "Chicago ",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -87.782995,
+          41.940739
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Crystal Edmonds ",
+        "Age": 32,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Fatal Gun Shot ",
+        "imAgeurl ": "https://i.ytimg.com/vi/_ngiSUaJjM0/maxresdefault.jpg",
+        "City_State ": "Baltimore ",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.631479,
+          39.302158
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Jazz Alfort ",
+        "Age": 30,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Fatal Gun Shot ",
+        "imAgeurl ": "https://thumbs.mic.com/ZTZmYTY3N2RjNCMvOFhRNlFhcHo5Sm9nMGZFNy1RdjFkTkZaeWpnPS8yeDE3NjoxNTk4eDk4OS8xNjAweDkwMC9maWx0ZXJzOmZvcm1hdChqcGVnKTpxdWFsaXR5KDgwKS9odHRwczovL3MzLmFtYXpvbmF3cy5jb20vcG9saWN5bWljLWltYWdlcy95cXV0bW1lb3V0emsyYWUyenNjdXRlenVhem85aHNtbTg5YjhkYmNuMmE1ZW91cGs2bHlhbWxuNWVvbTFidnZwLmpwZw.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -86.816006,
+          33.511638
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Brandi Bledsoe ",
+        "Age": 32,
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Fata Gun Shot",
+        "imAgeurl ": "http://www.advocate.com/sites/advocate.com/files/2016/10/14/brandi-bledsoe-x750.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -81.692155,
+          41.4874
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "Noony Norwood ",
+        "Age": "Unknown",
+        "ETHNICITY": "Black ",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Fatal Gun Shot ",
+        "imAgeurl ": "https://thumbs.mic.com/M2NlYzAwYWE1MyMvd3llc1FYUnpvOTVhZVJQc0tyMU42M1Y3Rk93PS81OHgxODoyMzQyeDExODIvODAweDQ1MC9maWx0ZXJzOmZvcm1hdChqcGVnKTpxdWFsaXR5KDgwKS9odHRwczovL3MzLmFtYXpvbmF3cy5jb20vcG9saWN5bWljLWltYWdlcy9jaWxoMmp0ejVvMGw4Zjg3cHo2c2huem5hZHRkNWRzcXQyeWZucmdubGhqN3pjYm5kdXJheHJiazY5ZG5vMnV2LmpwZw.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -77.504942,
+          37.5000068
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "Name": "India Moore ",
+        "Age": 29,
+        "ETHNICITY": "Black",
+        "Identity": "Transgender Womyn ",
+        "TypeofDeath": "Fatal Gun Shot ",
+        "imAgeurl ": "https://thumbs.mic.com/MjQxMWRmZTAwOCMvZGRIclBpd1pNNDVua0FValI2SnZPeWF6U2E4PS81OHgxODoyMzQyeDExODIvMTYwMHg5MDAvZmlsdGVyczpmb3JtYXQoanBlZyk6cXVhbGl0eSg4MCkvaHR0cHM6Ly9zMy5hbWF6b25hd3MuY29tL3BvbGljeW1pYy1pbWFnZXMvY2lsaDJqdHo1bzBsOGY4N3B6NnNobnpuYWR0ZDVkc3F0MnlmbnJnbmxoajd6Y2JuZHVyYXhyYms2OWRubzJ1di5qcGc.jpg",
+        "City_State ": "",
+        "Symbol ": ""
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          -76.531811,
+          37.005409
+        ]
+      }
+    }
+  ]
+};// JavaScript Document
